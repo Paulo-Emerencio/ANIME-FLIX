ac44d901995e9823de41457f9127ec45bc979980
Paulo Emerencio <p.emerencio@gmail.com>
2021-04-11 23:46:56 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+/* const div = document.querySelector(".main-movie");
+
+function changeAnime(aniNumber) {
+    div.insertAdjacentHTML("afterbegin", 
+    `
+    <div class="container">
+        <h3 class="title">${aniNumber}</h3>
+        <p class="description">Gon é um jovem que vive em uma pequena ilha. Seu sonho é tornar-se um Hunter como o pai, para procurar tesouros, artefatos e criaturas míticas. Determinado a se tornar um Hunter profissional e a colocar sua vida em risco ao desafiar o desconhecido, ele parte em uma jornada e faz três amigos: Killua, Kurapika e Leorio.</p>
+        <div class="buttons">
+            <a class="btn" href="#" target="_blank">
+                <i class="fas fa-play"></i>
+                Assistir Trailer
+            </a>
+            <a class="btn" href="#" target="_blank">
+                <i class="fas fa-plus"></i>
+                Minha Lista
+            </a>
+        </div>
+    </div>
+    `);
+} */
