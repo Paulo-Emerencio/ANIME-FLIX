578fc672bd331943138827376729c49186be8f6a
Paulo Emerencio <p.emerencio@gmail.com>
2021-04-12 12:28:20 -0300

    js work in progress

---
+++ js/animeInfo.js
@@ -0,0 +1,24 @@
+const titulos = [
+    'Hunter X Hunter',
+    'Attack On Titan', 
+    'Steins;Gate',
+    'Death Note',
+    'High Score Girl',
+    'One Punch Man',
+    'Ascendance Of A Bookworm',
+    '3-gatsu no Lion',
+    'Dorohedoro',
+    'Kill la Kill',
+];
+const desc = [
+    'Gon é um jovem que vive em uma pequena ilha. Seu sonho é tornar-se um Hunter como o pai, para procurar tesouros, artefatos e criaturas míticas. Determinado a se tornar um Hunter profissional e a colocar sua vida em risco ao desafiar o desconhecido, ele parte em uma jornada e faz três amigos: Killua, Kurapika e Leorio.',
+    'Séculos atrás, a humanidade foi abatida até quase a extinção por monstruosas criaturas humanóides chamadas titãs, forçando os remanecentes a se esconderem de medo atrás de enormes muralhas, resultando em cem anos de paz. No entanto, essa frágil calma é quebrada quando um titã colossal consegue romper a parede externa supostamente inexpugnável, reacendendo a luta pela sobrevivência contra as abominações devoradoras de humanos.',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+];
--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-/* const div = document.querySelector(".main-movie");
+const mainAnime = document.querySelector(".main-anime");
+mainAnime.style.backgroundImage = `linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.7)), url(img/big1.jpg)`;
 
-function changeAnime(aniNumber) {
-    div.insertAdjacentHTML("afterbegin", 
-    `
-    <div class="container">
-        <h3 class="title">${aniNumber}</h3>
-        <p class="description">Gon é um jovem que vive em uma pequena ilha. Seu sonho é tornar-se um Hunter como o pai, para procurar tesouros, artefatos e criaturas míticas. Determinado a se tornar um Hunter profissional e a colocar sua vida em risco ao desafiar o desconhecido, ele parte em uma jornada e faz três amigos: Killua, Kurapika e Leorio.</p>
-        <div class="buttons">
-            <a class="btn" href="#" target="_blank">
-                <i class="fas fa-play"></i>
-                Assistir Trailer
-            </a>
-            <a class="btn" href="#" target="_blank">
-                <i class="fas fa-plus"></i>
-                Minha Lista
-            </a>
-        </div>
-    </div>
-    `);
-} */
+const title = document.querySelector(".title");
+title.innerText = titulos[0];
+
+const description = document.querySelector(".description");
+description.innerHTML = desc[0];
+
+const trailer = document.querySelector("#trailer");
+trailer.href="https://www.youtube.com/watch?v=d6kBeJjTGnY";
+
+const list = document.querySelector("#list");
+list.href="https://myanimelist.net/anime/11061/Hunter_x_Hunter_2011";
+
+function changeAnime(animeNumber) {
+    mainAnime.style.backgroundImage = 
+    `linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.7)), url(img/big${animeNumber}.jpg)`;
+    title.innerText = titulos[animeNumber - 1];
+    description.innerText = desc[animeNumber - 1];
+}
