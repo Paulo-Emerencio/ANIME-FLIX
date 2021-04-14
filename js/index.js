fdbe542e967264650c6cbabb16caed0c80da8dbf
Paulo Emerencio <p.emerencio@gmail.com>
2021-04-14 12:00:34 -0300

    js fadein

--- js/index.js
+++ js/index.js
@@ -13,10 +13,26 @@ trailer.href = trailers[0];
 const list = document.querySelector("#list");
 list.href = lista[0];
 
-function changeAnime(animeNumber) {
-    mainAnime.style.backgroundImage = `linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.7)), url(img/big${animeNumber}.jpg)`;
-    title.innerText = titulos[animeNumber - 1];
-    description.innerText = desc[animeNumber - 1];
-    trailer.href = trailers[animeNumber - 1];
-    list.href = lista[animeNumber - 1];
+function fadeIn(elemento) {
+    let op = 0;
+    let timer = setInterval(function () {
+        if (op >= 100) clearInterval(timer);
+        elemento.style.filter = `opacity(${op}%)`;
+        op += 2.5;
+    }, 8);
 }
+
+let lastAnimeNumber = 1;
+function changeAnime(animeNumber) {
+    if (animeNumber != lastAnimeNumber) {
+        mainAnime.style.filter = 'opacity(0%)';
+        mainAnime.style.backgroundImage = `linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.7)), url(img/big${animeNumber}.jpg)`;
+        title.innerText = titulos[animeNumber - 1];
+        description.innerText = desc[animeNumber - 1];
+        trailer.href = trailers[animeNumber - 1];
+        list.href = lista[animeNumber - 1];
+        fadeIn(mainAnime);
+        lastAnimeNumber = animeNumber;
+    }
+}
+
